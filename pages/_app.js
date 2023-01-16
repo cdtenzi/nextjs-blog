838fdd8fb442c410ab69113013de1e6fa2a45250
Daniel <cdtenzi@gmail.com>
2023-01-16 16:06:23 +0100

    resolución de conflictos de merge

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,6 @@
 // `pages/_app.js`
-<<<<<<< HEAD
-import '../styles/global.css';
+import "../styles/global.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-=======
-import "../styles/global.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
->>>>>>> 5987e50363c6416168312ac94788e9472b155191
